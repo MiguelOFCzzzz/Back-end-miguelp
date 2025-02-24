75113d813875b6ad83b95641004613a3563442d1
MiguelOFCzzzz <169094569+MiguelOFCzzzz@users.noreply.github.com>
2025-02-24 17:14:54 -0300

    upgrade aula 3

--- Aula 3/index.js
+++ Aula 3/index.js
@@ -1,1 +1,28 @@
-dasd
+const express = require("express");
+const userService = require ("./userService");
+
+const app = express();
+app.use(express.json());
+
+//rota para criar usuario
+
+app.post("/users", (req, res)=>{
+    const {nome, email} = req.body;
+    if(!nome || !email){
+        return res.status(400).json
+        ({error: "nome e email são obrigatorios"})
+    }
+
+    const user = userService.addUser(nome, email);
+    res.status(200).json({user});
+})
+
+//rota para listar todos os arquivos
+app.get("/users", (req, res)=>{
+    res.json(userService.getUsers());
+});
+
+const port = 3000;
+app.listen(port,() =>{
+    console.log("Servidor rodando na porta:", port);
+})
