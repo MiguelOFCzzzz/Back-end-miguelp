75f8ed815d4c125d6f0d477e31d1821afb5a24c2
MiguelOFCzzzz <mp254914@gmail.com>
2025-02-25 11:17:46 -0300

    mudancas 25/02

---
+++ aula 3/user.js
@@ -0,0 +1,9 @@
+class User {
+  constructor (Id,nome,email){
+    this.Id = Id; //id do usuario
+    this.mome = nome; //nome do usuario
+    this.email = email; //email do usuario
+  }
+}
+
+module.exports = User; // exportar o modulo
