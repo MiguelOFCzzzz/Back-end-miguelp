f6779cffbbac38b3ccc2d9a8ad9902d757a02b67
MiguelOFCzzzz <169094569+MiguelOFCzzzz@users.noreply.github.com>
2025-02-25 11:20:09 -0300

    upgrade

--- Aula 3/index.js
+++ Aula 3/index.js
@@ -1,28 +1,31 @@
-const express = require("express");
-const userService = require ("./userService");
+const express = require('express')
+const userService = require('./userService')
 
-const app = express();
-app.use(express.json());
+const app = express (); // nome qualquer para express
+app.use(express.json()); // vou habilitar json no express 
 
-//rota para criar usuario
+// rota para criar usuario
 
-app.post("/users", (req, res)=>{
-    const {nome, email} = req.body;
-    if(!nome || !email){
-        return res.status(400).json
-        ({error: "nome e email são obrigatorios"})
-    }
+app.post("/users",(req,res) =>{
+  const {nome, email, senha, endereco, telefone, cpf} = req.body;
+  if (!nome || !email || !senha || !endereco || !telefone || !cpf) {
+      return res.status(400).json
+      ({error: "Nome e email são obrigatórios"})
 
-    const user = userService.addUser(nome, email);
-    res.status(200).json({user});
+
+  }
+  const user = userService.addUser(nome,email,senha, endereco, telefone, cpf);
+  res.status(200).json({user});
 })
 
-//rota para listar todos os arquivos
-app.get("/users", (req, res)=>{
-    res.json(userService.getUsers());
-});
+//rota para listar todos os usuarios
 
-const port = 3000;
-app.listen(port,() =>{
-    console.log("Servidor rodando na porta:", port);
+app.get("/users", (req,res)=>{
+ res.json(userService.getUsers());
+})
+
+const port = 3000
+app.listen(port,()=>{
+    console.log("Servidor rodando na porta", port);
+
 })
--- Aula 3/user.js
+++ Aula 3/user.js
@@ -1,5 +1,13 @@
-class User{ constructor(id, nome, email){
-     this.id = id;  // id do usuario
-      this.nome = nome; // nome do usuario 
-      this.email = email; // email do usuario 
-      } } module.exports = User; // exportar o arquivo
+class User {
+  constructor (id,nome,email,senha,endereco,telefone,cpf){
+    this.id = id; //id do usuario
+    this.Nome = nome; //nome do usuario
+    this.email = email; //email do usuario
+    this.senha = senha; //senha do usuário
+    this.endereco = endereco; //Endereço do usuário
+    this.telefone = telefone // telefone do usuário
+    this.cpf = cpf; //CPF do usuário
+  }
+}
+
+module.exports = User; // exportar o modulo
